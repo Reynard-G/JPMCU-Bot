f038bad11eb826d161a4ddc1ece3fcab102ada38
Reynard-G <reynard.ean.gunawan@gmail.com>
2023-01-01 11:45:01 -0600

    register 2 accounts bug & comments

--- modals/credentials.js
+++ modals/credentials.js
@@ -6,6 +6,23 @@ module.exports = {
     permissions: [],
     run: async (client, interaction) => {
         try {
+            // Check if user is already registered
+            const userExists = (await client.query(`SELECT 1 FROM users WHERE name = '${interaction.user.id}';`));
+            if ((Object.keys(userExists).length > 0) && (userExists[0].hasOwnProperty('1'))) {
+                return await interaction.reply({
+                    ephemeral: true,
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle('Registration Failed')
+                            .setDescription(`You are already registered with JPMCU. If you believe this is an error, please contact a staff member by opening a ticket.`)
+                            .setColor('Red')
+                            .setTimestamp()
+                            .setFooter({ text: `JPMCU`, iconURL: interaction.guild.iconURL() })
+                    ]
+                });
+            }
+
+            // Assign user details
             const id = parseInt((((await client.query(`SELECT MAX(id) FROM users;`))[0]['MAX(id)']).toString()).replace('n', '')) + 1;
             const accountNumber = parseInt((((await client.query(`SELECT MAX(account_number) FROM users;`))[0]['MAX(account_number)']).toString()).replace('n', '')) + 1;
             const date = new Date().toISOString().slice(0, 19).replace('T', ' ');
@@ -40,6 +57,38 @@ module.exports = {
 
             try {
                 await client.query(`INSERT INTO users (id, name, email, phone, account_number, user_type, role_id, branch_id, status, profile_picture, email_verified_at, sms_verified_at, password, provider, provider_id, country_code, remember_token, created_at, updated_at, two_factor_code, two_factor_expires_at, otp, otp_expires_at, allow_withdrawal, document_verified_at, document_submitted_at) VALUES (${newUser.id}, '${newUser.name}', '${newUser.email}', ${newUser.phone}, ${newUser.account_number}, '${newUser.user_type}', ${newUser.role_id}, ${newUser.branch_id}, ${newUser.status}, '${newUser.profile_picture}', ${newUser.email_verified_at}, ${newUser.sms_verified_at}, '${newUser.password}', ${newUser.provider}, ${newUser.provider_id}, ${newUser.country_code}, ${newUser.remember_token}, '${newUser.created_at}', '${newUser.updated_at}', ${newUser.two_factor_code}, ${newUser.two_factor_expires_at}, ${newUser.otp}, ${newUser.otp_expires_at}, ${newUser.allow_withdrawal}, ${newUser.document_verified_at}, ${newUser.document_submitted_at});`);
+
+                // Assign the user to the member role
+                const memberRole = interaction.guild.roles.cache.get('1025591402102526072');
+                await interaction.member.roles.add(memberRole);
+
+                // Send the user their account credentials
+                await interaction.user.send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle('Account Credentials')
+                            .setDescription(`Your account credentials are as follows:` +
+                                `\n` +
+                                `\n**Username:** ${newUser.email}` +
+                                `\n**Password:** ${interaction.fields.getTextInputValue('passwordInput')}`
+                            )
+                            .setColor('#2F3136')
+                            .setTimestamp()
+                            .setFooter({ text: `JPMCU`, iconURL: interaction.user.avatarURL() })
+                    ]
+                });
+
+                return await interaction.reply({
+                    ephemeral: true,
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle('Registration Successful')
+                            .setDescription(`Your account has been successfully registered. Please check your DMs for your account credentials.`)
+                            .setColor('Green')
+                            .setTimestamp()
+                            .setFooter({ text: `JPMCU`, iconURL: interaction.guild.iconURL() })
+                    ]
+                });
             } catch (error) {
                 console.log(error);
                 return await interaction.reply({
@@ -54,38 +103,6 @@ module.exports = {
                     ]
                 });
             }
-
-            // Assign the user to the member role
-            const memberRole = interaction.guild.roles.cache.get('1025591402102526072');
-            await interaction.member.roles.add(memberRole);
-
-            // Send the user their account credentials
-            await interaction.user.send({
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle('Account Credentials')
-                        .setDescription(`Your account credentials are as follows:` +
-                            `\n` +
-                            `\n**Username:** ${newUser.email}` +
-                            `\n**Password:** ${interaction.fields.getTextInputValue('passwordInput')}`
-                        )
-                        .setColor('#2F3136')
-                        .setTimestamp()
-                        .setFooter({ text: `JPMCU`, iconURL: interaction.user.avatarURL() })
-                ]
-            });
-
-            return await interaction.reply({
-                ephemeral: true,
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle('Registration Successful')
-                        .setDescription(`Your account has been successfully registered. Please check your DMs for your account credentials.`)
-                        .setColor('Green')
-                        .setTimestamp()
-                        .setFooter({ text: `JPMCU`, iconURL: interaction.guild.iconURL() })
-                ]
-            });
         } catch (error) {
             console.log(error);
             return await interaction.reply({
--- slashCommands/user/register.js
+++ slashCommands/user/register.js
@@ -7,6 +7,7 @@ module.exports = {
     cooldown: 3000,
     run: async (client, interaction) => {
         try {
+            // Check if user is already registered
             const userExists = (await client.query(`SELECT 1 FROM users WHERE name = '${interaction.user.id}';`));
             if ((Object.keys(userExists).length > 0) && (userExists[0].hasOwnProperty('1'))) {
                 return await interaction.reply({
@@ -20,31 +21,32 @@ module.exports = {
                             .setFooter({ text: `JPMCU`, iconURL: interaction.guild.iconURL() })
                     ]
                 });
-            } else {
-                const contractEmbed = new EmbedBuilder()
-                    .setTitle('JPMCU Account Registration')
-                    .setDescription('By clicking the "I Agree" button, you agree to the following **Member Contract**:\n\n' +
-                        'https://docs.google.com/document/d/1V5ucuAc_41dCgEIeNsMfWUUtdP6rwk4u6bG7iTpys5w/edit?usp=sharing')
-                    .setColor('#2F3136')
-                    .setFooter({ text: 'JPMCU', iconURL: interaction.guild.iconURL() })
-                    .setTimestamp();
-
-                const agreeButton = new ButtonBuilder()
-                    .setCustomId('agreeContract_button')
-                    .setLabel('I Agree')
-                    .setStyle('Success');
-
-                const disagreeButton = new ButtonBuilder()
-                    .setCustomId('disagreeContract_button')
-                    .setLabel('I Disagree')
-                    .setStyle('Danger');
-
-                const buttonRow = new ActionRowBuilder()
-                    .addComponents(agreeButton, disagreeButton);
-
-                await interaction.reply({ embeds: [contractEmbed], components: [buttonRow], ephemeral: true });
-                return module.exports = { contractEmbed, buttonRow };
             }
+
+            const contractEmbed = new EmbedBuilder()
+                .setTitle('JPMCU Account Registration')
+                .setDescription('By clicking the "I Agree" button, you agree to the following **Member Contract**:\n\n' +
+                    'https://docs.google.com/document/d/1V5ucuAc_41dCgEIeNsMfWUUtdP6rwk4u6bG7iTpys5w/edit?usp=sharing')
+                .setColor('#2F3136')
+                .setFooter({ text: 'JPMCU', iconURL: interaction.guild.iconURL() })
+                .setTimestamp();
+
+            const agreeButton = new ButtonBuilder()
+                .setCustomId('agreeContract_button')
+                .setLabel('I Agree')
+                .setStyle('Success');
+
+            const disagreeButton = new ButtonBuilder()
+                .setCustomId('disagreeContract_button')
+                .setLabel('I Disagree')
+                .setStyle('Danger');
+
+            const buttonRow = new ActionRowBuilder()
+                .addComponents(agreeButton, disagreeButton);
+
+            await interaction.reply({ embeds: [contractEmbed], components: [buttonRow], ephemeral: true });
+            return module.exports = { contractEmbed, buttonRow };
+
         } catch (err) {
             console.log(err);
             return await interaction.reply({
